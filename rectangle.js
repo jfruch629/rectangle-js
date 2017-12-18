a1a48d7ee7e1da680e4ef291568ad29e1517373d
Jake Fruci <jfruch629@gmail.com>
2017-12-18 18:26:46 -0500

    JS practice with class-functions

---
+++ rectangle.js
@@ -0,0 +1,16 @@
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+
+    if (height) {
+      this.height = height;
+    } else {
+      this.height = width;
+    }
+  }
+
+
+  area() {
+    return this.width * this.height;
+  }
+}
